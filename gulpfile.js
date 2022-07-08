6ebaa10341f5e9a9816ce873449f4d4e590bfc05
Sagar Gadekar <sagar.gadekar@contentstack.com>
2022-07-08 15:50:02 +0530

    Fixed build error 'Cannot use import statement outside a module'

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-import { task, src, dest } from 'gulp';
-import inlinesource from 'gulp-inline-source';
-import replace from 'gulp-replace';
+const gulp = require("gulp");
+const inlinesource = require("gulp-inline-source");
+const replace = require("gulp-replace");
 
-task('default', () => {
-  return src('./build/*.html')
-    .pipe(replace('.js"></script>', '.js" inline></script>'))
-    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
-    .pipe(
-      inlinesource({
-        compress: false,
-        ignore: ['png', 'svg', 'jpg'],
-      }),
-    )
-    .pipe(dest('./build'));
+gulp.task("default", () => {
+	return gulp
+		.src("./build/*.html")
+		.pipe(replace('.js"></script>', '.js" inline></script>'))
+		.pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+		.pipe(
+			inlinesource({
+				compress: false,
+				ignore: ["png", "svg", "jpg"],
+			})
+		)
+		.pipe(gulp.dest("./build"));
 });
