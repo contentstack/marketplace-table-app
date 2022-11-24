6d0449bace3004c4966987043849e6b6348c7756
MaitriGurey1 <maitri.gurey@contentstack.com>
2022-11-24 14:01:49 +0530

    dataDog RUM implementation 1.have added dataDog RUM error tracking 2.have added App metaData

--- src/components/ErrorBoundary/index.tsx
+++ src/components/ErrorBoundary/index.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { TrackJS } from 'trackjs';
 
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+  applicationId: `${process.env.REACT_APP_DATADOG_RUM_APPLICATION_ID}`,
+  clientToken: `${process.env.REACT_APP_DATADOG_RUM_CLIENT_TOKEN}`,
+  site: 'datadoghq.com',
+  // service: 'my-web-application',
+  service: 'marketplace-table-app',
+  // Specify a version number to identify the deployed version of your application in Datadog
+  //version: "1",
+  sampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'mask-user-input',
+  useCrossSiteSessionCookie: true,
+});
+//captures and visually replay users' web browsing experience for review
+datadogRum.startSessionReplayRecording();
+
 interface MyProps {
   children: React.ReactElement;
 }
@@ -14,7 +35,11 @@ TrackJS.install({
   application: process.env.REACT_APP_TRACKER_ENV,
   console: { display: process.env.NODE_ENV === 'development' },
 });
-
+//adding metadata to datadogRUM
+datadogRum.addAction('App Metadata', {
+  application_type: 'marketplace',
+  application_name: 'Table App',
+});
 TrackJS.addMetadata('application_type', 'marketplace');
 TrackJS.addMetadata('application_name', 'Table App');
 
@@ -22,6 +47,8 @@ class ErrorBoundary extends React.Component<MyProps, MyState> {
   componentDidCatch(error: any) {
     // error tracker for error reporting service
     TrackJS.track(error);
+    //error tracking by dataDog RUM
+    datadogRum.addError(error);
   }
 
   render() {
--- src/hooks/useJsErrorTracker.tsx
+++ src/hooks/useJsErrorTracker.tsx
@@ -1,10 +1,14 @@
 import { TrackJS } from 'trackjs';
+import { datadogRum } from '@datadog/browser-rum';
+
 const useJsErrorTracker = () => {
   const addMetadata = (key: string, value: string) => {
     TrackJS.addMetadata(key, value);
   };
   const trackError = (error: any) => {
     TrackJS.track(error);
+    //error tracking by dataDog RUM
+    datadogRum.addError(error);
   };
   return { addMetadata, trackError };
 };
